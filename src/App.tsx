866bfe56364b8e925afe772b7f1452875087cb62
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2025-08-05 21:08:28 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import ruRu from "antd/locale/ru_RU";
 import { useEffect } from "react";
 import { themeConfig } from "./themeConfig";
 
-console.log(import.meta.env.VITE_APP_ENVIRONMENT);
-
 function App() {
   useEffect(() => {
     Telegram.WebApp.enableClosingConfirmation();
